5c621ade967018299fb470aa472e3aba2872dec1
Taim-Amer <taim.amer.000@gmail.com>
2024-11-30 14:46:11 -0800

    Home Screen v2

--- lib/features/orders/views/home/home_screen.dart
+++ lib/features/orders/views/home/home_screen.dart
@@ -1,15 +1,20 @@
 import 'package:comprehensive_pharmacy_client_role/app.dart';
+import 'package:comprehensive_pharmacy_client_role/common/styles/spacing_styles.dart';
 import 'package:comprehensive_pharmacy_client_role/common/widgets/appbar/appbar.dart';
 import 'package:comprehensive_pharmacy_client_role/common/widgets/custom_shapes/containers/dotted_container.dart';
+import 'package:comprehensive_pharmacy_client_role/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:comprehensive_pharmacy_client_role/features/orders/views/home/widgets/home_header.dart';
 import 'package:comprehensive_pharmacy_client_role/features/orders/views/home/widgets/home_navbar.dart';
+import 'package:comprehensive_pharmacy_client_role/features/orders/views/home/widgets/upload_files_container.dart';
 import 'package:comprehensive_pharmacy_client_role/features/orders/views/order/widgets/general_appbar.dart';
 import 'package:comprehensive_pharmacy_client_role/features/orders/views/order/widgets/general_drawer.dart';
+import 'package:comprehensive_pharmacy_client_role/localization/keys.dart';
 import 'package:comprehensive_pharmacy_client_role/utils/constants/colors.dart';
 import 'package:comprehensive_pharmacy_client_role/utils/constants/enums.dart';
 import 'package:comprehensive_pharmacy_client_role/utils/constants/image_strings.dart';
 import 'package:comprehensive_pharmacy_client_role/utils/constants/sizes.dart';
 import 'package:comprehensive_pharmacy_client_role/utils/constants/text_strings.dart';
+import 'package:comprehensive_pharmacy_client_role/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -19,6 +24,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       floatingActionButton: const HomeNavbar(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -28,34 +34,30 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: TSizes.defaultSpace.w),
+          padding: TSpacingStyle.paddingWithAppBarHeight,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const HomeHeader(),
               TSizes.spaceBtwSections.verticalSpace,
-              TDottedContainer(
+              const UploadFilesContainer(),
+              TSizes.spaceBtwItems.verticalSpace,
+              TRoundedContainer(
                 width: double.infinity,
-                height: 195.h,
-                borderColor: TColors.primary,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(TImages.uploadIcon),
-                    TSizes.md.verticalSpace,
-                    Text(TEnglishTexts.uploadFile, textAlign: TextAlign.center, style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w500, fontSize: 14)),
-                    TSizes.sm.verticalSpace,
-                    Text(TEnglishTexts.supportedFiles, textAlign: TextAlign.center, style: Theme.of(context).textTheme.labelLarge?.copyWith(fontSize: 12)),
-                    TSizes.md.verticalSpace,
-                    SizedBox(
-                      child: ElevatedButton(
-                          onPressed: onPressed,
-                          child: child,
-                      ),
-                    )
-                  ],
+                height: 165.h,
+                backgroundColor: dark ? TColors.dark : TColors.lightGrey,
+                showBorder: dark ? true : false,
+                child: TextFormField(
+                  cursorColor: TColors.primary,
+                  enableInteractiveSelection: false,
+                  maxLines: 7,
+                  // textAlign: TextAlign.right,
+                  decoration: InputDecoration(
+                    hintText: TEnglishTexts.writeNote,
+                    border: const OutlineInputBorder(),
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
